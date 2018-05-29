2d33e60e9faba5737129d4be9b154a34323e288d
LeoLu <836257584@qq.com>
2018-05-30 01:27:07 +0800

    修改fragment

--- app/src/main/java/com/weike/data/business/home/HomeFragment.java
+++ app/src/main/java/com/weike/data/business/home/HomeFragment.java
@@ -1,8 +1,15 @@
 package com.weike.data.business.home;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.weike.data.R;
 import com.weike.data.base.BaseFragment;
 
 /**
@@ -12,4 +19,12 @@ import com.weike.data.base.BaseFragment;
  */
 public class HomeFragment extends BaseFragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,null);
+
+        return view;
+
+    }
 }
