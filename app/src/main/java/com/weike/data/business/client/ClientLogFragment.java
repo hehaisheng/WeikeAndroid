72eaa2622f7375bcc19b73500552e84548622aeb
LeoLu <836257584@qq.com>
2018-06-29 17:52:00 +0800

    代码提交  维客助手

--- app/src/main/java/com/weike/data/business/client/ClientLogFragment.java
+++ app/src/main/java/com/weike/data/business/client/ClientLogFragment.java
@@ -3,21 +3,30 @@ package com.weike.data.business.client;
 import android.annotation.SuppressLint;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.gson.reflect.TypeToken;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
 import com.weike.data.BR;
 import com.weike.data.R;
 import com.weike.data.adapter.BaseDataBindingAdapter;
 import com.weike.data.base.BaseFragment;
 import com.weike.data.base.BaseObserver;
 import com.weike.data.base.BaseResp;
+import com.weike.data.business.log.AddLogActivity;
 import com.weike.data.config.Config;
 import com.weike.data.model.req.GetClientLogByIdReq;
 import com.weike.data.model.resp.GetClientLogByIdResp;
 import com.weike.data.model.viewmodel.ClientLogItemVM;
 import com.weike.data.network.RetrofitFactory;
+import com.weike.data.util.ActivitySkipUtil;
+import com.weike.data.util.LogUtil;
 import com.weike.data.util.SignUtil;
+import com.weike.data.util.TimeUtil;
 import com.weike.data.util.TransformerUtils;
 
 import java.util.ArrayList;
@@ -28,7 +37,7 @@ import java.util.List;
  */
 
 @SuppressLint("ValidFragment")
-public class ClientLogFragment extends BaseFragment {
+public class ClientLogFragment extends BaseFragment implements View.OnClickListener {
 
     private RecyclerView recyclerView;
 
@@ -36,11 +45,16 @@ public class ClientLogFragment extends BaseFragment {
 
     private BaseDataBindingAdapter adapter;
 
+    private SmartRefreshLayout smartRefreshLayout;
+
+
+
     private String clientId;
 
     @SuppressLint("ValidFragment")
     public ClientLogFragment(String clientId) {
         this.clientId = clientId;
+        LogUtil.d("acthome","id:" + clientId);
     }
 
     @Override
@@ -51,15 +65,39 @@ public class ClientLogFragment extends BaseFragment {
     @Override
     protected void loadFinish(View view) {
 
+
+
+        adapter = new BaseDataBindingAdapter(getContext(),R.layout.widget_log_list_item,vms, BR.clientLogItemVM);
+        TextView textView = view.findViewById(R.id.tv_add_log);
+        if (TextUtils.isEmpty(clientId)){
+            textView.setVisibility(View.GONE);
+        }
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivitySkipUtil.skipAnotherAct(getActivity(),AddLogActivity.class);
+            }
+        });
+        recyclerView = view.findViewById(R.id.recycler_log_list);
+        smartRefreshLayout = view.findViewById(R.id.smartrefreshlayout);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+
+        loadData(true,1);
+
+    }
+
+    public void loadData(boolean isFist,final int page){
         GetClientLogByIdReq req = new GetClientLogByIdReq();
         req.id = clientId;
-        req.page = 1;
+        req.page = page;
         req.sign = SignUtil.signData(req);
 
         RetrofitFactory.getInstance().getService().postAnything(req, Config.GET_LOG_BY_ID)
-        .compose(TransformerUtils.jsonCompass(new TypeToken<BaseResp<GetClientLogByIdResp>>(){
+                .compose(TransformerUtils.jsonCompass(new TypeToken<BaseResp<GetClientLogByIdResp>>(){
 
-        })).subscribe(new BaseObserver<BaseResp<GetClientLogByIdResp>>() {
+                })).subscribe(new BaseObserver<BaseResp<GetClientLogByIdResp>>() {
             @Override
             protected void onSuccess(BaseResp<GetClientLogByIdResp> getClientLogByIdRespBaseResp) throws Exception {
                 for(int i = 0 ; i < getClientLogByIdRespBaseResp.getDatas().getLogList().size();i++){
@@ -67,7 +105,13 @@ public class ClientLogFragment extends BaseFragment {
                     ClientLogItemVM vm = new ClientLogItemVM();
                     vm.content.set(bean.getContent());
                     vm.time.set(bean.getLogDate());
+                    if(i == getClientLogByIdRespBaseResp.getDatas().getLogList().size() - 1) {
+                        vm.isShowLine.set(false);
+                    } else {
+                        vm.isShowLine.set(true);
+                    }
                     vms.add(vm);
+
                 }
                 adapter.notifyDataSetChanged();
 
@@ -78,13 +122,10 @@ public class ClientLogFragment extends BaseFragment {
 
             }
         });
-
-        adapter = new BaseDataBindingAdapter(getContext(),R.layout.widget_log_list_item,vms, BR.clientLogItemVM);
-        recyclerView = view.findViewById(R.id.recycler_log_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapter);
-
     }
 
+    @Override
+    public void onClick(View view) {
 
+    }
 }
